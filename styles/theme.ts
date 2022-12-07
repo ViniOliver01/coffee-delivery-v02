07a466d4d950166c9c537f08ab048b7843fc5604
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-12-07 11:07:36 -0300

    added new colors

--- styles/theme.ts
+++ styles/theme.ts
@@ -3,17 +3,34 @@ import { extendTheme } from "@chakra-ui/react";
 export const theme = extendTheme({
   colors: {
     yellow: {
-      "500": "#FFBA08",
+      dark: "#C47F17",
+      mid: "#DBAC2C",
+      light: "#F1E9C9",
     },
-    gray: {
-      "700": "#47585B",
-      "600": "#999999",
-      "100": "#DADADA",
-      "50": "#F5F8FA",
+    purple: {
+      dark: "#4B2995",
+      mid: "#8047F8",
+      light: "#EBE5F9",
+    },
+    green: {
+      dark: "#0A7441",
+      mid: "#0CA85D",
+      light: "#92FFCA",
+    },
+    base: {
+      title: "#272221",
+      subtitle: "#403937",
+      text: "#574F4D",
+      label: "#8D8686",
+      hover: "#D7D5D5",
+      button: "#E6E5E5",
+      input: "#EDEDED",
+      card: "#F3F2F2",
+      background: "#FAFAFA",
     },
   },
   fonts: {
-    heading: "Baloo",
+    heading: "Baloo 2",
     body: "Roboto",
   },
   styles: {
